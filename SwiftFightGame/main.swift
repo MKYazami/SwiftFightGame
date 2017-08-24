9cb817edeb84b8e09b09ad7327ec477f081b4d5b
Mehdi <mehdiyazami@gmail.com>
2017-08-24 16:43:14 +0200

    Initial Commit

---
+++ SwiftFightGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftFightGame
+//
+//  Created by Mehdi on 24/08/2017.
+//  Copyright © 2017 Medev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
