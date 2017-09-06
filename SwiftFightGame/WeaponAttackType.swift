166aed8006cd537afbf8446a892c126284aa7063
Mehdi <mehdiyazami@gmail.com>
2017-09-06 09:36:03 +0200

    Add Weapon.swift, WeaponAttack.swift, WeaponAttackType.swift, WeaponCare.swift, WeaponCareType.swift files

---
+++ SwiftFightGame/WeaponAttackType.swift
@@ -0,0 +1,9 @@
+//
+//  WeaponAttackType.swift
+//  SwiftFightGame
+//
+//  Created by Mehdi on 06/09/2017.
+//  Copyright © 2017 Medev. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftFightGame/WeaponCareType.swift
@@ -0,0 +1,9 @@
+//
+//  WeaponCareType.swift
+//  SwiftFightGame
+//
+//  Created by Mehdi on 06/09/2017.
+//  Copyright © 2017 Medev. All rights reserved.
+//
+
+import Foundation
